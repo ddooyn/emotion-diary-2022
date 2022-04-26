2ca7208a6e9c590673054ced3d96151b2789a09d
Sooyoung Yoon <ayoooo240@gmail.com>
2022-04-26 19:06:31 +0900

    ✨ state 관리 로직 추가 및 Context 세팅

--- src/App.js
+++ src/App.js
@@ -3,13 +3,79 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
 import Router from "./Router";
+import React, { useReducer, useRef } from "react";
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((item) => item.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((item) =>
+        item.id === action.data.id ? { ...action.data } : item
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 const App = () => {
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
+
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
   return (
     <HelmetProvider>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Router />
+        <DiaryStateContext.Provider value={data}>
+          <DiaryDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
+            <Router />
+          </DiaryDispatchContext.Provider>
+        </DiaryStateContext.Provider>
       </ThemeProvider>
     </HelmetProvider>
   );
