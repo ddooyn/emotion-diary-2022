d9de6f77065b6b416b6f5e08ec231acd967b1e03
Sooyoung Yoon <ayoooo240@gmail.com>
2022-06-05 17:01:43 +0900

    ✨ Home 페이지 헤더 한 달씩 날짜 이동 기능 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
-import PageTitle from "../components/PageTitle";
-import styled from "styled-components";
+import { useState } from 'react';
+import styled from 'styled-components';
+import PageTitle from '../components/PageTitle';
+import Header from '../components/Header';
+import Button from '../components/Button';
 
 const Home = () => {
+  const [curDate, setCurDate] = useState(new Date());
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  const increaseMonth = () =>
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+
+    const decreaseMonth = () =>
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+
   return (
     <>
-      <PageTitle title={"홈"} />
+      <PageTitle title={'홈'} />
+      <Header
+        headText={headText}
+        leftChild={<Button text={'<'} onClick={decreaseMonth} />}
+        rightChild={<Button text={'>'} onClick={increaseMonth} />}
+      />
       <HomeContainer className="App">
         <h1>Home</h1>
       </HomeContainer>
@@ -15,7 +36,6 @@ const Home = () => {
 export default Home;
 
 const HomeContainer = styled.section`
-  /* ${({ theme }) => theme.flex.centerCol}; */
   h1 {
     font-size: ${({ theme }) => theme.fontSizes.title};
   }
